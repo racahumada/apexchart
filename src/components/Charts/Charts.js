ca295bc3baf222ae1b18af40b4d891d33e4d6611
Ricardo Andres Castro Ahumada <ricardocastro.ti@gmail.com>
2021-01-28 07:15:47 -0300

    Hora yaxis

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import Chart from 'react-apexcharts';
 import { ChartWrapper } from '../../styles/Charts/style';
+import moment from 'moment';
+
+const sec = ['21.23', '01.23', '10.23', '15.03', '21.50'];
+
+const form = (value) => {
+  return moment.utc(value).format('HH:mm');
+};
 
 export default function Charts() {
   const [options, setOptions] = useState({
@@ -8,20 +15,35 @@ export default function Charts() {
       id: 'basic-bar',
       legend: { show: false },
     },
+    yaxis: {
+      labels: {
+        formatter: (value) => {
+          const oi = value.toString().split('.');
+          console.log(oi);
+          const teste = parseInt(oi[0]) + ':' + '00';
+          console.log(teste);
+          return teste;
+        },
+      },
+    },
     xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+      categories: [1991, 1992],
+      datetimeFormatter: {
+        hour: 'HH:mm',
+      },
     },
   });
 
   const [series, setSeries] = useState([
     {
       name: 'series-1',
-      data: [30, 40, 45, 50, 49, 60, 70, 91],
+      data: sec,
     },
-    {
-      name: 'series-2',
-      data: [30, 40, 45, 50, 49, 60, 70, 91],
-    },
+
+    // {
+    //   name: 'series-2',
+    //   data: [30, 40, 45, 50, 49, 60, 70, 91],
+    // },
   ]);
 
   return (
